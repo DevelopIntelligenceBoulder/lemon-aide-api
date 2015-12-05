63969bf3ae62d6b37c8570229b429ee7a68f08ae
kamrenz <kamren@developintelligence.com>
2015-12-04 20:47:30 -0700

    Updating APIs

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # lemon-aide-api
+Very basic mock data for building buttons and report data
---
+++ api.js
@@ -0,0 +1,70 @@
+module.exports = function(app) {
+    
+    //Our Transaction Model
+    var transaction = require('./transactions.js');
+
+    // Get product costs
+    app.get('/data/product/costs',
+        function(req, res) {
+            console.log('get product costs');
+
+            var costs = {
+                'largeLemonade': 3, 
+                'mediumLemonade': 2,
+                'healthySnack': 1,
+                'treat': 1
+            };
+
+            res.json(costs); 
+
+    });
+
+    // Get the complete list of products
+    app.get('/data/products',
+        function(req, res) {
+            console.log('list of products');
+
+            //List of products 
+            var products = [
+                {
+                    id: 0,
+                    domId: 'largeLemonade',
+                    description: 'Large lemonade',
+                    icon: 'ion-happy-outline',
+                    cost: 3
+                }, {
+                    id: 1,
+                    domId: 'mediumLemonade',
+                    description: 'Medium lemonade',
+                    icon: 'ion-sad-outline',
+                    cost: 2
+                }, {
+                    id: 2,
+                    domId: 'healthySnack',
+                    description: 'Healthy Snack',
+                    icon: 'ion-heart',
+                    cost: 1
+                }, {
+                    id: 3,
+                    domId: 'treat',
+                    description: 'Treat',
+                    icon: 'ion-heart-broken',
+                    cost: 1
+                }
+            ];
+
+          res.json(products); 
+
+    });
+
+    // Get all the transactions.
+    app.get('/data/transactions',
+        function(req, res) {
+            console.log('list of transactions');
+
+            res.json(transaction.getTransactions());
+    });
+
+};
+
+
