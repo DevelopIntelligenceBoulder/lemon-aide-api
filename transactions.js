95d1942192a873975f2b6754f5289614b919249e
kamrenz <kamren@developintelligence.com>
2015-12-06 12:04:50 -0700

    Updating ids

--- transactions.js
+++ transactions.js
@@ -3,191 +3,229 @@
  **/
 //List of transactions
 var transactions = [{
+	    id: 0,
 		time: '2015-01-01',
 		quantity: '600',
 		netSale: '750.00',
 		cost: '400.00'
 	}, {
+		id: 1,
 		time: '2015-02-01',
 		quantity: '100',
 		netSale: '250.00',
 		cost: '175.00'
 	}, {
+		id: 2,
 		time: '2015-03-01',
 		quantity: '100',
 		netSale: '250.00',
 		cost: '150.00'
 	}, {
+		id: 3,
 		time: '2015-04-01',
 		quantity: '425',
 		netSale: '750.00',
 		cost: '400.00'
 	}, {
+		id: 4,
 		time: '2015-05-01',
 		quantity: '300',
 		netSale: '450.00',
 		cost: '250.00'
 	}, {
+		id: 5,
 		time: '2015-06-01',
 		quantity: '650',
 		netSale: '850.00',
 		cost: '650.00'
 	}, {
+		id: 6,
 		time: '2015-07-01',
 		quantity: '100',
 		netSale: '350.00',
 		cost: '250.00'
 	}, {
+		id: 7,
 		time: '2015-08-01',
 		quantity: '100',
 		netSale: '350.00',
 		cost: '250.00'
 	}, {
+		id: 8,
 		time: '2015-09-01',
 		quantity: '300',
 		netSale: '450.00',
 		cost: '250.00'
 	}, {
+		id: 9,
 		time: '2015-10-01',
 		quantity: '300',
 		netSale: '550.00',
 		cost: '350.00'
 	}, {
+		id: 10,
 		time: '2015-11-01',
 		quantity: '650',
 		netSale: '750.00',
 		cost: '450.00'
 	}, {
+		id: 11,
 		time: '2015-12-01',
 		quantity: '300',
 		netSale: '450.00',
 		cost: '250.00'
 	}, {
+		id: 12,
 		time: '2014-01-01',
 		quantity: '650',
 		netSale: '850.00',
 		cost: '650.00'
 	}, {
+		id: 13,
 		time: '2014-02-01',
 		quantity: '100',
 		netSale: '350.00',
 		cost: '150.00'
 	}, {
+		id: 14,
 		time: '2014-03-01',
 		quantity: '100',
 		netSale: '250.00',
 		cost: '150.00'
 	}, {
+		id: 15,
 		time: '2014-04-01',
 		quantity: '500',
 		netSale: '350.00',
 		cost: '250.00'
 	}, {
+		id: 16,
 		time: '2014-05-01',
 		quantity: '500',
 		netSale: '750.00',
 		cost: '550.00'
 	}, {
+		id: 17,
 		time: '2014-06-01',
 		quantity: '650',
 		netSale: '850.00',
 		cost: '550.00'
 	}, {
+		id: 18,
 		time: '2014-07-01',
 		quantity: '300',
 		netSale: '550.00',
 		cost: '350.00'
 	}, {
+		id: 19,
 		time: '2014-08-01',
 		quantity: '100',
 		netSale: '350.00',
 		cost: '250.00'
 	}, {
+		id: 20,
 		time: '2014-09-01',
 		quantity: '150',
 		netSale: '450.00',
 		cost: '150.00'
 	}, {
+		id: 21,
 		time: '2014-10-01',
 		quantity: '500',
 		netSale: '750.00',
 		cost: '400.00'
 	}, {
+		id: 22,
 		time: '2014-11-01',
 		quantity: '425',
 		netSale: '650.00',
 		cost: '300.00'
 	}, {
+		id: 23,
 		time: '2014-12-01',
 		quantity: '300',
 		netSale: '450.00',
 		cost: '300.00'
 	}, {
+		id: 24,
 		time: '2013-01-01',
 		quantity: '600',
 		netSale: '750.00',
 		cost: '400.00'
 	}, {
+		id: 25,
 		time: '2013-02-01',
 		quantity: '100',
 		netSale: '250.00',
 		cost: '175.00'
 	}, {
+		id: 26,
 		time: '2013-03-01',
 		quantity: '100',
 		netSale: '250.00',
 		cost: '150.00'
 	}, {
+		id: 27,
 		time: '2013-04-01',
 		quantity: '425',
 		netSale: '750.00',
 		cost: '400.00'
 	}, {
+		id: 28,
 		time: '2013-05-01',
 		quantity: '300',
 		netSale: '450.00',
 		cost: '250.00'
 	}, {
+		id: 29,
 		time: '2013-06-01',
 		quantity: '650',
 		netSale: '850.00',
 		cost: '650.00'
 	}, {
+		id: 30,
 		time: '2013-07-01',
 		quantity: '100',
 		netSale: '350.00',
 		cost: '250.00'
 	}, {
+		id: 31,
 		time: '2013-06-01',
 		quantity: '100',
 		netSale: '350.00',
 		cost: '250.00'
 	}, {
+		id: 32,
 		time: '2013-07-01',
 		quantity: '300',
 		netSale: '450.00',
 		cost: '250.00'
 	}, {
+		id: 33,
 		time: '2013-08-01',
 		quantity: '300',
 		netSale: '550.00',
 		cost: '350.00'
 	}, {
+		id: 34,
 		time: '2013-09-01',
 		quantity: '650',
 		netSale: '750.00',
 		cost: '450.00'
 	}, {
+		id: 35,
 		time: '2013-10-01',
 		quantity: '300',
 		netSale: '450.00',
 		cost: '250.00'
 	}, {
+		id: 36,
 		time: '2013-11-01',
 		quantity: '650',
 		netSale: '850.00',
 		cost: '650.00'
 	}, {
+		id: 37,
 		time: '2013-12-01',
 		quantity: '100',
 		netSale: '350.00',
@@ -199,191 +237,229 @@ var transactions = [{
  **/
 //List of transactions
 var moreTransactions = [{
+	id: 38,
 	time: '2012-01-01',
 	quantity: '600',
 	netSale: '750.00',
 	cost: '400.00'
 }, {
+	id: 39,
 	time: '2012-02-01',
 	quantity: '100',
 	netSale: '250.00',
 	cost: '175.00'
 }, {
+	id: 40,
 	time: '2012-03-01',
 	quantity: '100',
 	netSale: '250.00',
 	cost: '150.00'
 }, {
+	id: 41,
 	time: '2012-04-01',
 	quantity: '425',
 	netSale: '750.00',
 	cost: '400.00'
 }, {
+	id: 42,
 	time: '2012-05-01',
 	quantity: '300',
 	netSale: '450.00',
 	cost: '250.00'
 }, {
+	id: 43,
 	time: '2012-06-01',
 	quantity: '650',
 	netSale: '850.00',
 	cost: '650.00'
 }, {
+	id: 44,
 	time: '2012-07-01',
 	quantity: '100',
 	netSale: '350.00',
 	cost: '250.00'
 }, {
+	id: 45,
 	time: '2012-08-01',
 	quantity: '100',
 	netSale: '350.00',
 	cost: '250.00'
 }, {
+	id: 46,
 	time: '2012-09-01',
 	quantity: '300',
 	netSale: '450.00',
 	cost: '250.00'
 }, {
+	id: 47,
 	time: '2012-10-01',
 	quantity: '300',
 	netSale: '550.00',
 	cost: '350.00'
 }, {
+	id: 48,
 	time: '2012-11-01',
 	quantity: '650',
 	netSale: '750.00',
 	cost: '450.00'
 }, {
+	id: 49,
 	time: '2012-12-01',
 	quantity: '300',
 	netSale: '450.00',
 	cost: '250.00'
 }, {
+	id: 50,
 	time: '2011-01-01',
 	quantity: '650',
 	netSale: '850.00',
 	cost: '650.00'
 }, {
+	id: 51,
 	time: '2011-02-01',
 	quantity: '100',
 	netSale: '350.00',
 	cost: '150.00'
 }, {
+	id: 52,
 	time: '2011-03-01',
 	quantity: '100',
 	netSale: '250.00',
 	cost: '150.00'
 }, {
+	id: 53,
 	time: '2011-04-01',
 	quantity: '500',
 	netSale: '350.00',
 	cost: '250.00'
 }, {
+	id: 54,
 	time: '2011-05-01',
 	quantity: '500',
 	netSale: '750.00',
 	cost: '550.00'
 }, {
+	id: 55,
 	time: '2011-06-01',
 	quantity: '650',
 	netSale: '850.00',
 	cost: '550.00'
 }, {
+	id: 56,
 	time: '2011-07-01',
 	quantity: '300',
 	netSale: '550.00',
 	cost: '350.00'
 }, {
+	id: 57,
 	time: '2011-08-01',
 	quantity: '100',
 	netSale: '350.00',
 	cost: '250.00'
 }, {
+	id: 58,
 	time: '2011-09-01',
 	quantity: '150',
 	netSale: '450.00',
 	cost: '150.00'
 }, {
+	id: 59,
 	time: '2011-10-01',
 	quantity: '500',
 	netSale: '750.00',
 	cost: '400.00'
 }, {
+	id: 60,
 	time: '2011-11-01',
 	quantity: '425',
 	netSale: '650.00',
 	cost: '300.00'
 }, {
+	id: 61,
 	time: '2011-12-01',
 	quantity: '300',
 	netSale: '450.00',
 	cost: '300.00'
 }, {
+	id: 62,
 	time: '2010-01-01',
 	quantity: '600',
 	netSale: '750.00',
 	cost: '400.00'
 }, {
+	id: 63,
 	time: '2010-02-01',
 	quantity: '100',
 	netSale: '250.00',
 	cost: '175.00'
 }, {
+	id: 64,
 	time: '2010-03-01',
 	quantity: '100',
 	netSale: '250.00',
 	cost: '150.00'
 }, {
+	id: 65,
 	time: '2010-04-01',
 	quantity: '425',
 	netSale: '750.00',
 	cost: '400.00'
 }, {
+	id: 66,
 	time: '2010-05-01',
 	quantity: '300',
 	netSale: '450.00',
 	cost: '250.00'
 }, {
+	id: 67,
 	time: '2010-06-01',
 	quantity: '650',
 	netSale: '850.00',
 	cost: '650.00'
 }, {
+	id: 68,
 	time: '2010-07-01',
 	quantity: '100',
 	netSale: '350.00',
 	cost: '250.00'
 }, {
+	id: 69,
 	time: '2010-06-01',
 	quantity: '100',
 	netSale: '350.00',
 	cost: '250.00'
 }, {
+	id: 70,
 	time: '2010-07-01',
 	quantity: '300',
 	netSale: '450.00',
 	cost: '250.00'
 }, {
+	id: 71,
 	time: '2010-08-01',
 	quantity: '300',
 	netSale: '550.00',
 	cost: '350.00'
 }, {
+	id: 72,
 	time: '2010-09-01',
 	quantity: '650',
 	netSale: '750.00',
 	cost: '450.00'
 }, {
+	id: 73,
 	time: '2010-10-01',
 	quantity: '300',
 	netSale: '450.00',
 	cost: '250.00'
 }, {
+	id: 74,
 	time: '2010-11-01',
 	quantity: '650',
 	netSale: '850.00',
 	cost: '650.00'
 }, {
+	id: 75,
 	time: '2010-12-01',
 	quantity: '100',
 	netSale: '350.00',
